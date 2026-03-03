ed4f85cb074e10290fdac83151fb9f88125df9cc
Robin Lemmers <r.lemmers@outlook.com>
2026-03-03 19:59:50 +0100

    footer component gemaakt in plaats van iedere page hetzelfde doen

--- public/contact.php
+++ public/contact.php
@@ -44,16 +44,9 @@
             </p>
         </section>
     </main>
-    <footer>
-        <p class="copyright">
-            <a href="https://no-ai-icon.com/statement/?url=(robinlemmers.com)" target="_blank">
-                <img class="no-ai" src="img/no-ai-icon-01.png" alt="Icon dat aantoont dat ik geen AI heb gebruikt"></a>&amp; &copy; 2026 Robin Lemmers
-        </p>
-        <ul class="footerLinks">
-            <li><a href="https://github.com/SuperTNT17" target="_blank">GitHub <i class="fa-solid fa-arrow-up-right-from-square"></i></a></li>
-            <li><a href="https://www.linkedin.com/in/robin-lemmers-ab2539250/" target="_blank">LinkedIn <i class="fa-solid fa-arrow-up-right-from-square"></i></a></li>
-        </ul>
-    </footer>
+    <?php
+        include_once("components/footer.php");
+    ?>
 </body>
 
 </html>
